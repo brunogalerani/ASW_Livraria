e8b9bc98ae25cd1ec07630eca3e010775d5cb938
Ruan Silva <ruanluiz.as@gmail.com>
2016-11-26 00:28:39 -0200

    commit3

---
+++ src/database/JPAUtil.java
@@ -0,0 +1,12 @@
+package database;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("livrariadb");
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+}
