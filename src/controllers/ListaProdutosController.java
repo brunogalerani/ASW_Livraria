b6b911b940ab195986fadd4d4aaf54e3cdb13971
camila-borges <camila_nami@hotmail.com>
2016-11-27 02:46:44 -0200

    .

--- src/controllers/ListaProdutosController.java
+++ src/controllers/ListaProdutosController.java
@@ -8,20 +8,53 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class ListaProdutosController implements Initializable{
-	@FXML
-	private ListView listViewProdutos;
+	
 	@FXML
 	private Button buttonCadastrar, buttonEditar, buttonApagar, buttonDetalhes, buttonVoltar;
 	@FXML
 	private Label labelNome, labelCodigoDeBarras, labelMarca, labelPreco;
+	
+	@FXML
+	private TableView tableViewProduto;
+	
+	@FXML
+	private TableColumn tableColumnQtdDisponivel, tableColumnNome;
 	
 	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@FXML
+	public void handleBtnCadastrar(){
+		
+	}
+	
+	@FXML
+	public void handleBtnEditar(){
+		
+	}
+	
+	@FXML
+	public void handleBtnDetalhes(){
+		
+	}
+	
+	@FXML
+	public void handleBtnApagar(){
+		
+	}
+	
+	@FXML
+	public void handleBtnVoltar(){
 		
 	}
 	
