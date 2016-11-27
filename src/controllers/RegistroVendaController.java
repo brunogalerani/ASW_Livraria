c1241b00728225b4663424cfecd318ea692b10ec
camila-borges <camila_nami@hotmail.com>
2016-11-27 01:57:53 -0200

    Registro de vendas

---
+++ src/controllers/RegistroVendaController.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class RegistroVendaController implements Initializable{
+
+	@FXML
+	private Button buttonConcluir, buttonVoltar, buttonAdicionar, buttonRemover;
+	
+	@FXML
+	private TextField textFieldQuantidade, textFieldCPF;
+	
+	@FXML
+	private Label labelNomeProduto;
+	
+	@FXML
+	private ListView listViewProdutosComprando, listViewProdutosDisponiveis;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+	public void handleBtnConcluir(){
+		
+	}
+	
+	@FXML
+	public void handleBtnVoltar(){
+		
+	}
+	
+	@FXML
+	public void handleBtnAdicionar(){
+		
+	}
+	
+	@FXML
+	public void handleBtnRemover(){
+		
+	}
+
+}
