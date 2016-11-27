145bd62c724938d6faa1cf294f29d6900d513685
camila-borges <camila_nami@hotmail.com>
2016-11-27 02:15:29 -0200

    selecionar clientes/registrar vendas

---
+++ src/controllers/SelecionarClienteController.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class SelecionarClienteController implements Initializable{
+
+	@FXML
+	private Button buttonCadastrar, buttonVoltar, buttonConcluir;
+
+	@FXML
+	private TableColumn tableColumnCPF, tableColumnNome;
+	
+	@FXML
+	private TableView tableViewClientes;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+	public void handleBtnConcluir(){
+		
+	}
+	
+	@FXML
+	public void handleBtnVoltar(){
+		
+	}
+	
+	@FXML
+	public void handleBtnCadastrar(){
+		
+	}
+
+}
