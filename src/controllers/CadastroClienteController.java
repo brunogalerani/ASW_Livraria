67eb140f94b73a8e14aa79e50d22aa14769b4e1d
Ruan Silva <ruanluiz.as@gmail.com>
2016-11-27 01:08:33 -0200

    commit3

--- src/controllers/CadastroClienteController.java
+++ src/controllers/CadastroClienteController.java
@@ -1,25 +1,34 @@
 package controllers;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import dao.ClienteDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import models.Cliente;
 
 public class CadastroClienteController  implements Initializable{
 
 	@FXML
-	private TextField textFieldNome, textFieldDataNascimento, textFieldCPF, textFieldRG, textFieldTelefone,
+	private TextField textFieldNome, textFieldCPF, textFieldRG, textFieldTelefone,
 		textFieldEmail, textFieldCEP, textFieldEndereco, textFieldRua, textFieldNumero, textFieldBairro,
 		textFieldComplemento, textFieldEstado, textFieldCidade;
 	@FXML
+	private DatePicker datePickerDataNascimento;
+	@FXML
 	private Button buttonVoltar, buttonCadastrar;
+	
+	private Cliente cliente;
+	private ClienteDAO clienteDAO;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		this.clienteDAO = new ClienteDAO();
 		
 	}
 	
@@ -27,7 +36,10 @@ public class CadastroClienteController  implements Initializable{
 	private void handleBtnCadastrar() {
 		
 	}
-	
+	private void cadastrar() {
+		String nome = this.textFieldNome.getText();
+		LocalDate dataNascimento = 
+	}
 	@FXML
 	private void handleBtnVoltar() {
 		
