9298bcb8424ccbfe72983bf8e5d5b6efaa973b47
Ruan Silva <ruanluiz.as@gmail.com>
2016-11-26 23:12:29 -0200

    CommitController

--- src/controllers/CadastroProdutoDispositivoMovelController.java
+++ src/controllers/CadastroProdutoDispositivoMovelController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -38,10 +39,28 @@ public class CadastroProdutoDispositivoMovelController  implements Initializable
 	}
 	private void cadastrarCelular() {
 		this.celular = new Celular();
+		boolean touchScreen;
+		double tamanhoTela = Double.parseDouble(this.textFieldTamanhoTela.getText());
+		String tipoChip = this.textFieldTipoChip.getText();
+		int quantidadeChip = Integer.parseInt(this.textFieldQuantidadeChip.getText());
+		String tecnologiaSuportada = this.textFieldTecnologiaSuportada.getText();
+		String resolucao = this.textFieldResolucao.getText();
+		String cor = this.textFieldCor.getText();
+		double altura = Double.parseDouble(this.textFieldAltura.getText());
+		double largura = Double.parseDouble(this.textFieldLargura.getText());
+		double profundidade = Double.parseDouble(this.textFieldProfundidade.getText());
+		double potencia = Double.parseDouble(this.textFieldPotencia.getText());
+		int alimentacao = Integer.parseInt(this.textFieldAlimentacao.getText());
+		double consumo = Double.parseDouble(this.textFieldConsumo.getText());
+		boolean bateria;
+		LocalDate garantia;
 		String modelo = this.textFieldModelo.getText();
+		String marca = this.textFieldMarca.getText();
+		String fabricante = this.textFieldFabricante.getText();
+		double armazenamento = Double.parseDouble(this.textFieldArmazenamento.getText());
+		double peso = Double.parseDouble(this.textFieldPeso.getText());
 		double preco = Double.parseDouble(this.textFieldPreco.getText());
 		long codBarras = Long.parseLong(this.textFieldCodigoBarras.getText());
-		
 	}
 	@FXML
 	private void btnVoltar() {
