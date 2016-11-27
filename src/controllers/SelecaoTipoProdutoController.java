f7c640d464d38e9239fa84311a1c4eb356a9bf48
camila-borges <camila_nami@hotmail.com>
2016-11-27 16:58:36 -0200

    tela de seleção tipo de produto

---
+++ src/controllers/SelecaoTipoProdutoController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class SelecaoTipoProdutoController implements Initializable{
+
+	@FXML
+	private Button buttonVoltar, buttonConfirmar;
+	@FXML
+	private RadioButton radioButtonDM, radioButtonEB, radioButtonVG, radioButtonLF;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+	private void handleBtnVoltar(){
+		
+	}
+	
+	@FXML
+	private void handleBtnConfirmar(){
+		
+	}
+
+}
