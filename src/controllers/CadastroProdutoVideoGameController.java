0cbbf8769a3e4638295965eec209f06048953240
Ruan Silva <ruanluiz.as@gmail.com>
2016-11-28 10:50:45 -0200

    CommitAlteracao10

--- src/controllers/CadastroProdutoVideoGameController.java
+++ src/controllers/CadastroProdutoVideoGameController.java
@@ -46,7 +46,19 @@ public class CadastroProdutoVideoGameController  implements Initializable{
 		this.textFieldBateria.setText(this.videoGame.getBateria());
 		this.textFieldCodigoBarras.setText(String.valueOf(this.videoGame.getCodBarras()));
 		this.textFieldConsumo.setText(String.valueOf(this.videoGame.getConsumo()));
-		
+		this.textFieldCor.setText(this.videoGame.getCor());
+		this.textFieldFabricante.setText(this.videoGame.getFabricante());
+		this.textFieldLargura.setText(String.valueOf(this.videoGame.getLargura()));
+		this.textFieldMarca.setText(this.videoGame.getMarca());
+		this.textFieldMidia.setText(this.videoGame.getMidia());
+		this.textFieldModelo.setText(this.videoGame.getModelo());
+		this.textFieldNome.setText(this.videoGame.getNome());
+		this.textFieldPeso.setText(String.valueOf(this.videoGame.getPeso()));
+		this.textFieldPlataformaSuportada.setText(this.videoGame.getPlataformaSuportada());
+		this.textFieldPotencia.setText(String.valueOf(this.videoGame.getPotencia()));
+		this.textFieldPreco.setText(String.valueOf(this.videoGame.getPreco()));
+		this.textFieldProfundidade.setText(String.valueOf(this.videoGame.getProfundidade()));
+		this.datePickerGarantia.setValue(this.videoGame.getGarantia());
 	}
 	@FXML
 	private void handleBtnCadastrar() {
